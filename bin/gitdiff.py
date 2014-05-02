1db2bee0bbe790bc38aaa18a4c0a9aff3f9410a9
Chris Mumford <cmumford@cmumford.com>
2014-05-02 10:47:04 -0700

    gitdiff script now compatible with Windows
    Assuming you have Meld installed on Windows.
    See meld-installer project on SourceForge.

--- bin/gitdiff.py
+++ bin/gitdiff.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import sys
 import os
+import subprocess
+import sys
 
-isMac = sys.platform == 'darwin'
-if isMac:
-    # Run FileMerge directly as it blocks and opendiff does not
+if sys.platform == 'darwin':
+    # On OSX run FileMerge directly as it blocks and opendiff does not
     os.system('/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge -left "%s" -right "%s"' % (sys.argv[2], sys.argv[5]))
+elif sys.platform == 'win32':
+    meld_path = r"%s\Meld\meld\meld.exe" % os.environ['PROGRAMFILES(X86)']
+    subprocess.check_call([meld_path, sys.argv[2], sys.argv[5]])
 else:
     os.system('meld "%s" "%s"' % (sys.argv[2], sys.argv[5]))
