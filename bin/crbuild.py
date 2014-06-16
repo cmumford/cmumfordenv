1eaef2851598b3abd5d576324f9519b76662ba30
Chris Mumford <cmumford@cmumford.com>
2014-06-16 13:08:41 -0700

    crbuild: Building nacl

--- bin/crbuild.py
+++ bin/crbuild.py
@@ -412,7 +412,6 @@ class Options(object):
     self.gyp = GypValues()
     self.debug = False
     self.release = False
-    self.gyp.gyp_defines.add('disable_nacl=1')
     if platform.system() == 'Windows':
       # Should read in chromium.gyp_env and append to those values
       self.gyp.gyp_defines.add('component=shared_library')
