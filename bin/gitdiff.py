14fce12d4f7052b4864472e8c239f1dd8b8a03d4
Chris Mumford <cmumford@cmumford.com>
2014-07-01 09:22:05 -0700

    gitdiff: Using enums for cmd line args

--- bin/gitdiff.py
+++ bin/gitdiff.py
@@ -4,11 +4,26 @@ import os
 import subprocess
 import sys
 
+class Params:
+    PROG = 0
+    FILE_PATH = 1
+    BASE_PATH = 2
+    BASE_HASH = 3
+    BASE_MODE = 4
+    LOCAL_PATH = 5
+    LOCAL_HASH = 6
+    LOCAL_MODE = 7
+
 if sys.platform == 'darwin':
     # On OSX run FileMerge directly as it blocks and opendiff does not
-    os.system('/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge -left "%s" -right "%s"' % (sys.argv[2], sys.argv[5]))
-elif sys.platform == 'win32':
-    meld_path = r"%s\Meld\meld\meld.exe" % os.environ['PROGRAMFILES(X86)']
-    subprocess.check_call([meld_path, sys.argv[2], sys.argv[5]])
+    cmd = ['/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge',
+           '-left', sys.argv[Params.BASE_PATH], '-right', sys.argv[Params.LOCAL_PATH]]
 else:
-    os.system('meld "%s" "%s"' % (sys.argv[2], sys.argv[5]))
+    # Use meld
+    if sys.platform == 'win32':
+        meld_path = r"%s\Meld\meld\meld.exe" % os.environ['PROGRAMFILES(X86)']
+    else:
+        meld_path = 'meld'
+    cmd = [meld_path, sys.argv[Params.BASE_PATH], sys.argv[Params.LOCAL_PATH]]
+
+subprocess.check_call(cmd)
