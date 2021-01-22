0f3af903a6c46005ec3943dc6dfa6c14ab806287
Chris Mumford <cmumford@cmumford.com>
2021-01-22 09:09:48 -0800

    Trimmed trailing whitespace.

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,45 +1,45 @@
-############################################################################## 
-## 
-## Invoke-CmdScript.ps1 
-## 
-## Invoke the specified batch file (and parameters), but also propigate any 
-## environment variable changes back to the PowerShell environment that 
-## called it. 
-## 
-## ie: 
-## 
-## PS > type foo-that-sets-the-FOO-env-variable.cmd 
-## @set FOO=%* 
-## echo FOO set to %FOO%. 
-##  
-## PS > $env:FOO 
-##  
+##############################################################################
+##
+## Invoke-CmdScript.ps1
+##
+## Invoke the specified batch file (and parameters), but also propigate any
+## environment variable changes back to the PowerShell environment that
+## called it.
+##
+## ie:
+##
+## PS > type foo-that-sets-the-FOO-env-variable.cmd
+## @set FOO=%*
+## echo FOO set to %FOO%.
+##
+## PS > $env:FOO
+##
 ## PS > Invoke-CmdScript "foo-that-sets-the-FOO-env-variable.cmd" Test
-##  
-## C:\Temp>echo FOO set to Test. 
-## FOO set to Test. 
-##  
-## PS > $env:FOO 
-## Test 
-## 
-############################################################################## 
+##
+## C:\Temp>echo FOO set to Test.
+## FOO set to Test.
+##
+## PS > $env:FOO
+## Test
+##
+##############################################################################
 function Invoke-CmdScript( [string] $script, [string] $parameters )
 {
-	$tempFile = [IO.Path]::GetTempFileName() 
+	$tempFile = [IO.Path]::GetTempFileName()
 
-	## Store the output of cmd.exe.  We also ask cmd.exe to output  
-	## the environment table after the batch file completes 
+	## Store the output of cmd.exe.  We also ask cmd.exe to output
+	## the environment table after the batch file completes
 
 	cmd /c " `"$script`" $parameters && set > `"$tempFile`" "
 
-	## Go through the environment variables in the temp file. 
-	## For each of them, set the variable in our local environment. 
-	Get-Content $tempFile | Foreach-Object {  
-		if($_ -match "^(.*?)=(.*)$") 
+	## Go through the environment variables in the temp file.
+	## For each of them, set the variable in our local environment.
+	Get-Content $tempFile | Foreach-Object {
+		if($_ -match "^(.*?)=(.*)$")
 		{
-			Set-Content "env:\$($matches[1])" $matches[2] 
+			Set-Content "env:\$($matches[1])" $matches[2]
 		}
-	} 
+	}
 
 	Remove-Item $tempFile
 }
