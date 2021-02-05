bed805c27351aab934ee3cb48b84730b8264d5c5
Chris Mumford <cmumford@cmumford.com>
2021-02-05 09:32:30 -0800

    crsync: Windows => Python 3.9.

--- bin/crsync.bat
+++ bin/crsync.bat
@@ -3,4 +3,4 @@ setlocal
 :: This is required with cygwin only.
 PATH=%~dp0;%PATH%
 set PYTHONDONTWRITEBYTECODE=1
-call "%USERPROFILE%\AppData\Local\Programs\Python\Python38\python.exe" "%~dp0crsync.py" %*
+call "%LOCALAPPDATA%\Programs\Python\Python39\python.exe" "%~dp0crsync.py" %*
